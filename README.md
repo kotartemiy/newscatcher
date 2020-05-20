e91a96a968de2cb128699bf8ef4831edd23279b1
kotartemiy <bugara.artem@gmail.com>
2020-05-20 22:04:28 +0300

    v 0.2.0 pre poetry

--- README.md
+++ README.md
@@ -1,69 +1,206 @@
 # Newscatcher
-Programmatically collect normalized news from (almost) any website.
-By [newscatcherapi.com](https://www.newscatcherapi.com).
+**Programmatically collect normalized news from (almost) any website.**
+
+Filter by **topic**, **country**, or **language**.
+
+By [newscatcherapi.com](https://www.newscatcherapi.com) (*this package is fully 
+self-sufficient, you can just use it. No dependency on external services/API*)
 
 ## Demo
 ![](newscatcherdemo.gif)
 
 ## Motivation
-While working on newscatcherapi -- JSON API to query the news articles,
+While working on [newscatcherapi](https://newscatcherapi.com/) - JSON API 
+to query the news articles,
 I came up with an idea to make a simple Python package that would allow
 to easily grab the live news data. 
 
 When I used to be a junior data scientist working on my own side projects,
 it was difficult for me to operate with external data sources. I knew Python
 quite well, but in most cases it was not enough to build proper data pipelines
-that required gathering data on my own. 
+that required gathering data on my own. I hope that this package will help you 
+with your next project. 
 
-Even though I do not recommend to use this package for any production systems, I believe that it should be enough to test your assumptions and build some MVPs.
+Even though I do not recommend to use this package for any production systems, 
+I believe that it should be enough to test your assumptions and build some MVPs.
 
 ## Installation
-`pip install newscatcher`
+`pip install newscatcher --upgrade` 
+
+
+## Quick Start
+```python
+from newscatcher import Newscatcher
+```
+
+Get the latest news from [nytimes.com](https://www.nytimes.com/) 
+(_we support thousands of news websites, try yourself!_) main news feed
+```python
+nc = Newscatcher(website = 'nytimes.com')
+results = nc.get_news()
+
+# results.keys()
+# 'url', 'language', 'country', 'articles'
+
+# Get the articles
+articles = results['articles']
+
+first_article_summary = articles[0]['summary']
+first_article_title = articles[0]['title']
+```
+
+Get the latest news from [nytimes.com](https://www.nytimes.com/) **politics** feed
+
+```python
+nc = Newscatcher(website = 'nytimes.com', topic = 'politics')
+
+results = nc.search()
+articles = results['articles']
+```
+
+There is a limited set of topic that you might find:
+
+``` 'tech', 'news', 'business', 'science', 'finance', 'food', 'politics', 'economics', 'travel', 'entertainment', 'music', 'sport', 'world' ```
+
+However, not all topics are supported by every newspaper.
+
+How to check which topics are supported by which newspaper:
+```python
+from newscatcher import describe_url
+
+describe = describe_url('nytimes.com')
+
+print(describe['topics'])
+```
+
+
+### Get the list of all news feeds by topic/language/country
+If you want to find the full list of supported news websites 
+you can always do so using `urls()` function
+```python
+from newscatcher import urls
+
+# URLs by TOPIC
+politic_urls = urls(topic = 'politics')
+
+# URLs by COUNTRY
+american_urls = urls(country = 'US')
+
+# URLs by LANGUAGE
+english_urls = urls(language = 'en')
+
+# Combine any from topic, country, language
+american_english_politics_urls = urls(country = 'US', topic = 'politics', language = 'en') 
+
+# note some websites do not explicitly declare their language 
+# as a result they will be excluded from queries based on language
+```
+
+
+
+
+## Documentation
+
+### `Newscatcher` Class
+```python
+from newscatcher import Newscatcher
+
+nc = Newscatcher(website, topic = None)
+```
+**Please take the base form url of a website** (without `www.`,neither `https://`, nor `/` at the end of url).
+
+For example: “nytimes”.com, “news.ycombinator.com” or “theverge.com”.
+___
+`Newscatcher.get_news()` - Get the latest news from the website of interest.
+
+Allowed topics:
+`tech`, `news`, `business`, `science`, `finance`, `food`, 
+`politics`, `economics`, `travel`, `entertainment`, 
+`music`, `sport`, `world`
+
+If no topic is provided, the main feed is returned.
+
+Returns a dictionary of 5 elements:
+1. `url` - URL of the website
+2. `topic` - topic of the returned feed
+3. `language` - language of returned feed
+4. `country` - country of returned feed
+5. `articles` - articles of the feed. [Feedparser object]((https://pythonhosted.org/feedparser/reference.html))
+
+___
+
+`Newscatcher.get_headlines()` - Returns only the headlines
+
+___
+`Newscatcher.print_headlines(n)` - Print top `n` headlines
+
+
+<br> 
+<br> 
+<br> 
+
+### `describe_url()` & `urls()`
+Those functions exist to help you navigate through this package
+
+___
+```python
+from newscatcher import describe_url
+```
+
+`describe_url(website)` - Get the main info on the website. 
+
+Returns a dictionary of 5 elements:
+1. `url` - URL of the website
+2. `topics` - list of all supported topics
+3. `language` - language of website
+4. `country` - country of returned feed
+5. `main_topic` - main topic of a website
+
+___
+```python
+from newscatcher import urls
+```
+
+`urls(topic = None, language = None, country = None)` - Get a list of all supported 
+news websites given any combination of `topic`, `language`, `country`
+
+Returns a list of websites that match your combination of `topic`, `language`, `country`
+
+Supported topics:
+`tech`, `news`, `business`, `science`, `finance`, `food`, 
+`politics`, `economics`, `travel`, `entertainment`, 
+`music`, `sport`, `world`
+
+
+Supported countries:
+`US`, `GB`, `DE`, `FR`, `IN`, `RU`, `ES`, `BR`, `IT`, `CA`, `AU`, `NL`, `PL`, `NZ`, `PT`, `RO`, `UA`, `JP`, `AR`, `IR`, `IE`, `PH`, `IS`, `ZA`, `AT`, `CL`, `HR`, `BG`, `HU`, `KR`, `SZ`, `AE`, `EG`, `VE`, `CO`, `SE`, `CZ`, `ZH`, `MT`, `AZ`, `GR`, `BE`, `LU`, `IL`, `LT`, `NI`, `MY`, `TR`, `BM`, `NO`, `ME`, `SA`, `RS`, `BA`
+
+Supported languages:
+`EL`, `IT`, `ZH`, `EN`, `RU`, `CS`, `RO`, `FR`, `JA`, `DE`, `PT`, `ES`, `AR`, `HE`, `UK`, `PL`, `NL`, `TR`, `VI`, `KO`, `TH`, `ID`, `HR`, `DA`, `BG`, `NO`, `SK`, `FA`, `ET`, `SV`, `BN`, `GU`, `MK`, `PA`, `HU`, `SL`, `FI`, `LT`, `MR`, `HI`
+
+
+
+
 
 ## Tech/framework used
 The package itself is nothing more than a SQLite database with 
 RSS feed endpoints for each website and some basic wrapper of
-[feedparser][3].
+[feedparser](https://pythonhosted.org/feedparser/index.html).
 
-## Code Example/Documentation
-Let's review all possible usage of the package. 
 
-In its core, it has a class called *Newscatcher*. This class is all you need in order to get latest news.
+## About Us
+We are Newscatcher API team. We are glad that you liked our package.
 
-After installing your package, import the class:
+If you want to search for any news data, consider using [our API](https://newscatcherapi.com/)
 
-`from newscatcher import Newscatcher`
- 
-Now you just need to put a url of a desired news source as an input into our class. 
-**Please take the base form url of a website** (without `www.`,neither `https://`, nor `/` at the end of url).
+![](newscatcher_oneliner.png)
 
-For example: “nytimes”.com, “news.ycombinator.com” or “theverge.com”.
 
-`news_source = Newscatcher('blackfaldslife.com')`
+[Artem Bugara]() - co-founder of Newscatcher, made v.0.1.0
 
-If you have done it right and the source that you chose is presented in our database, you will get a variable with 3 components and 1 method:
+[Maksym Sugonyaka](https://www.linkedin.com/mwlite/in/msugonyaka) - co-founder of Newscatcher, made v.0.1.0
 
-- `news_source.website` -- the same string that you entered inside the class.
-- `news_source.news` -- a list of a feedparser dictionary with latest news presented on the website. 
-- `news_source.headlines` -- a list with latest headlines presented on the website.
-- `news_source.print_headlines()` -- print headlines of all latest articles.
-
-Each element of *news* list is a json object with all relevant and available information regarding an article. If you want to know more about the attributes that you can extract from this json, go check the official documentation of feedparser following this link: [feedparser\_attributes][4]. You can find everything that begins with *entries[i]*. But be aware that not all the attributes are provided by the news website. 
-
-If for some reason you do not like classes, you can always import 2 main methods and use them separately.
-
-`from newscatcher import get_news`
-`news = get_news('wired.co.uk')`
-
-or
-
-`from newscatcher import get_headlines`
-`news = get_headlines('wired.co.uk')`
-
+[Becket Trotter](https://www.linkedin.com/in/beckettrotter/) - Python Developper, made v.0.2.0
 
 ## Licence
 MIT
-
-
-[3]:	%3Chttps://pythonhosted.org/feedparser/index.html%3E
-[4]:	%3Chttps://pythonhosted.org/feedparser/reference.html%3E
--- newscatcher/__init__.py
+++ newscatcher/__init__.py
@@ -1,70 +1,268 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
+# Retrieve and analyze
+# 24/7 streams of news data
+import sys
 import sqlite3
-import requests
+#import requests
 import feedparser
-from urllib.parse import urlsplit
 import pkg_resources
+from tldextract import extract
 
-DB_FILE = pkg_resources.resource_filename('newscatcher', 'data/newscatcher_package_v2.db')
+DB_FILE = pkg_resources.resource_filename('newscatcher', 'data/package_rss.db')
+
+
+class Query:
+    # Query class used to build subsequent sql queries
+    def __init__(self):
+        self.params = {'website': None, 'topic': None}
+
+    def build_conditional(self, field, sql_field):
+        # single conditional build
+        field = field.lower()
+        sql_field = sql_field.lower()
+
+        if self.params[field] != None:
+            conditional = "{} = '{}'".format(sql_field, self.params[field])
+            return conditional
+        return
+
+    def build_where(self):
+        # returning the conditional from paramters
+        # the post "WHERE"
+        conditionals = []
+
+        conv = {'topic': 'topic_unified', 'website': 'clean_url'}
+
+        for field in conv.keys():
+            cond = self.build_conditional(field, conv[field])
+            if cond != None:
+                conditionals.append(cond)
+
+        if conditionals == []:
+            return
+
+        conditionals[0] = 'WHERE ' + conditionals[0]
+        conditionals = ''' AND '.join([x for x in conditionals if x != None])
+		+ ' ORDER BY IFNULL(Globalrank,999999);'''
+
+        return conditionals
+
+    def build_sql(self):
+        # build sql on user qeury
+        db = sqlite3.connect(DB_FILE, isolation_level=None)
+        sql = 'SELECT rss_url from rss_main ' + self.build_where()
+
+        db.close()
+        return sql
+
+
+def clean_url(dirty_url):
+    # website.com
+    dirty_url = dirty_url.lower()
+    o = extract(dirty_url)
+    return o.domain + '.' + o.suffix
+
 
 class Newscatcher:
-    
-    def __init__(self, website = 'news.ycombinator.com'):
-        self.website = website
-        self.news = get_news(website)
-        self.headlines = get_headlines(website)
-        
-    def print_headlines(self, n = None):
-        if n == None:
-            i = 1
-            for headline in self.headlines:
-                if i < 10:
-                    print(str(i) + '.   |  ' + headline )
-                    i += 1
-                elif i in list(range(10,100)):
-                    print(str(i) + '.  |  ' + headline )
-                    i += 1                
-                else:
-                    print(str(i) + '. |  ' + headline )
-                    i += 1       
-    
+    # search engine
+    def build_sql(self):
+        if self.topic is None:
+            sql = '''SELECT rss_url from rss_main 
+					 WHERE clean_url = '{}';'''
+            sql = sql.format(self.url)
+            return sql
+
+    def __init__(self, website, topic=None):
+        # init with given params
+        website = website.lower()
+        self.url = clean_url(website)
+        self.topic = topic
+
+    def get_headlines(self, n=None):
+        if self.topic is None:
+            sql = '''SELECT rss_url,topic_unified, language, clean_country from rss_main 
+					 WHERE clean_url = '{}' AND main = 1;'''
+            sql = sql.format(self.url)
         else:
-            i = 1
-            for headline in self.headlines:
-                if i < 10 and i <= n:
-                    print(str(i) + '.   |  ' + headline )
-                    i += 1
-                elif i in list(range(10,100)) and i <= n:
-                    print(str(i) + '.  |  ' + headline )
-                    i += 1                
-                elif i >= 100 and i <= n:
-                    print(str(i) + '. |  ' + headline )
-                    i += 1  
-        
-        
+            sql = '''SELECT rss_url, topic_unified, language, clean_country from rss_main 
+					 WHERE clean_url = '{}' AND topic_unified = '{}';'''
+            sql = sql.format(self.url, self.topic)
 
-def get_news(website = 'news.ycombinator.com'):
-    if type(website) != str:
-        raise TypeError("input must be a string") 
-    
+        db = sqlite3.connect(DB_FILE, isolation_level=None)
+
+        try:
+            rss_endpoint, topic, language, country = db.execute(sql).fetchone()
+            feed = feedparser.parse(rss_endpoint)
+        except:
+            if self.topic is not None:
+                sql = '''SELECT rss_url from rss_main 
+					 WHERE clean_url = '{}';'''
+                sql = sql.format(self.url)
+
+                if len(db.execute(sql).fetchall()) > 0:
+                    db.close()
+                    print('Topic is not supported')
+                    return
+                else:
+                    print('Website is not supported')
+                    return
+                    db.close()
+            else:
+                print('Website is not supported')
+                return
+
+        if feed['entries'] == []:
+            db.close()
+            print('\nNo headlines found check internet connection or query parameters\n')
+            return
+
+        title_list = []
+        for article in feed['entries']:
+            if 'title' in article:
+                title_list.append(article['title'])
+            if n != None:
+                if len(title_list) == n:
+                    break
+
+        return title_list
+
+    def print_headlines(self, n=None):
+        headlines = self.get_headlines(n)
+
+        i = 1
+        for headline in headlines:
+            if i < 10:
+                print(str(i) + '.   |  ' + headline)
+                i += 1
+            elif i in list(range(10, 100)):
+                print(str(i) + '.  |  ' + headline)
+                i += 1
+            else:
+                print(str(i) + '. |  ' + headline)
+                i += 1
+
+    def get_news(self, n=None):
+        # return results based on current stream
+        if self.topic is None:
+            sql = '''SELECT rss_url,topic_unified, language, clean_country from rss_main 
+					 WHERE clean_url = '{}' AND main = 1;'''
+            sql = sql.format(self.url)
+        else:
+            sql = '''SELECT rss_url, topic_unified, language, clean_country from rss_main 
+					 WHERE clean_url = '{}' AND topic_unified = '{}';'''
+            sql = sql.format(self.url, self.topic)
+
+        db = sqlite3.connect(DB_FILE, isolation_level=None)
+
+        try:
+            rss_endpoint, topic, language, country = db.execute(sql).fetchone()
+            feed = feedparser.parse(rss_endpoint)
+        except:
+            if self.topic is not None:
+                sql = '''SELECT rss_url from rss_main 
+					 WHERE clean_url = '{}';'''
+                sql = sql.format(self.url)
+
+                if len(db.execute(sql).fetchall()) > 0:
+                    db.close()
+                    print('Topic is not supported')
+                    return
+                else:
+                    print('Website is not supported')
+                    return
+                    db.close()
+            else:
+                print('Website is not supported')
+                return
+
+        if feed['entries'] == []:
+            db.close()
+            print('\nNo results found check internet connection or query parameters\n')
+            return
+
+        if n == None or len(feed['entries']) <= n:
+            articles = feepoetry initd['entries']  # ['summary']#[0].keys()
+        else:
+            articles = feed['entries'][:n]
+
+        db.close()
+        return {'url': self.url, 'main_topic': topic,
+                'language': language, 'country': country, 'articles': articles}
+
+
+def describe_url(website):
+    # return newscatcher fields that correspond to the url
+    website = website.lower()
+    website = clean_url(website)
     db = sqlite3.connect(DB_FILE, isolation_level=None)
-    try:
-        rss_endpont = db.execute('''select rss_endpoint from rss_table where url = \'''' +\
-                                 website +'''\';''').fetchone()[0]
-    except TypeError:
-        raise Exception('website is not supported')
-    feed = feedparser.parse(rss_endpont)
-    if feed['entries'] == []:
-        raise Exception('check internet connection / website is not supported')
-        
-    return feed['entries']
+
+    sql = "SELECT clean_url, language, clean_country, topic_unified from rss_main WHERE clean_url = '{}' and main == 1 ".format(
+        website)
+    results = db.execute(sql).fetchone()
+    main = results[-1]
+
+    if main == None:
+        print('\nWebsite not supported\n')
+        return
+
+    if len(main) == 0:
+        print('\nWebsite note supported\n')
+        return
+
+    sql = "SELECT DISTINCT topic_unified from rss_main WHERE clean_url == '{}'".format(website)
+    topics = db.execute(sql).fetchall()
+    topics = [x[0] for x in topics]
+
+    ret = {'url': results[0], 'language': results[1], 'country': results[2], 'main_topic': main, 'topics': topics}
+
+    return ret
 
 
-def get_headlines(website = 'news.ycombinator.com'):
-    feed_entries = get_news(website)
-    title_list = []
-    for article in feed_entries:
-        if 'title' in article:
-            title_list.append(article['title'])
-    return title_list
+def urls(topic=None, language=None, country=None):
+    # return urls that matches users parameters
+    if language != None:
+        language = language.lower()
+
+    if country != None:
+        country = country.upper()
+
+    if topic != None:
+        topic = topic.lower()
+
+    db = sqlite3.connect(DB_FILE, isolation_level=None)
+    quick_q = Query()
+    inp = {'topic': topic, 'language': language, 'country': country}
+    for x in inp.keys():
+        quick_q.params[x] = inp[x]
+
+    conditionals = []
+    conv = {'topic': 'topic_unified', 'website': 'clean_url',
+            'country': 'clean_country', 'language': 'language'}
+
+    for field in conv.keys():
+        try:
+            cond = quick_q.build_conditional(field, conv[field])
+        except:
+            cond = None
+
+        if cond != None:
+            conditionals.append(cond)
+
+    sql = ''
+
+    if conditionals == []:
+        sql = 'SELECT clean_url from rss_main '
+    else:
+        conditionals[0] = ' WHERE ' + conditionals[0]
+        conditionals = ' AND '.join([x for x in conditionals if x is not None])
+        conditionals += ' AND main = 1 ORDER BY IFNULL(Globalrank,999999);'
+        sql = 'SELECT DISTINCT clean_url from rss_main' + conditionals
+
+    ret = db.execute(sql).fetchall()
+    if len(ret) == 0:
+        print('\nNo websites found for given parameters\n')
+        return
+
+    db.close()
+    return [x[0] for x in ret]
